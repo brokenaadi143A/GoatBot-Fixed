8e606f3c303122ad6edadf6fb3c012e867445dbc
Priyanshi Kaur <sasmitapattnayak104@gmail.com>
2024-07-30 10:48:53 +0530

    Update 00_prefix.js

--- scripts/cmds/00_prefix.js
+++ scripts/cmds/00_prefix.js
@@ -18,9 +18,9 @@ module.exports = {
 Yo, my prefix is [ . ]\n
 𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:
 ➥ .help [number of page] -> see commands
-➥ .sim [message] -> talk to bot
+➥ .gt [message] -> talk to bot
 ➥ .callad [message] -> report any problem encountered
-➥ .help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/PriyanshiKaurJi`,
+➥ .help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/profile.php?id=100042292561861`,
  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/M4luPbE.gif")
  });
  }
